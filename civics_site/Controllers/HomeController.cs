60a7181f6244442865ad788d5ae7c0e3c209ed31
Mayooda <mayada.mahsoub@icstars-columbus.org>
2017-12-19 15:09:10 -0500

    Add project files.

---
+++ civics_site/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace civics_site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Language()
+        {
+            return View();
+        }
+        public ActionResult Economic()
+        {
+            return View();
+        }
+        public ActionResult Social()
+        {
+            return View();
+        }
+        public ActionResult Culture()
+        {
+            return View();
+        }
+        public ActionResult Donations()
+        {
+            return View();
+        }
+        public ActionResult statistics()
+        {
+            return View();
+        }
+    }
+}
---
+++ civics_site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="civics_site.MvcApplication" Language="C#" %>
